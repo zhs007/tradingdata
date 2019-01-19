11dff09e32b80c0939763d2f1abb60385e524aa0
zhs007 <sssxueren@gmail.com>
2019-01-19 11:00:14 +0800

    upd

--- bin/bitmex.js
+++ bin/bitmex.js
@@ -6,28 +6,26 @@ const process = require('process');
 let bitmex = new BitmexDataStream({
     addr: 'wss://www.bitmex.com/realtime',
     symbols: ['XBTUSD', 'XBTM19', 'XBTH19'],
+    funcOnClose: () => {
+        console.log('bitmex close');
+
+        bitmex.mgrTrade.finish(() => {
+            process.exit();
+        });
+    },
+    funcOnError: (err) => {
+        console.log('bitmex error ' + err);
+
+        bitmex.mgrTrade.finish(() => {
+            process.exit();
+        });
+    },
 });
-
-bitmex.cfg.funcOnClose = () => {
-    console.log('bitmex close');
-
-    bitmex.finish(() => {
-        process.exit();
-    });
-};
-
-bitmex.cfg.funcOnError = (err) => {
-    console.log('bitmex error ' + err);
-
-    bitmex.finish(() => {
-        process.exit();
-    });
-};
 
 process.on('unhandledRejection', (reason, p) => {
     console.log('error', 'Unhandled Rejection at: ' + p + ' reason: ' + reason);
 
-    bitmex.finish(() => {
+    bitmex.mgrTrade.finish(() => {
         process.exit();
     });
 });
@@ -35,7 +33,7 @@ process.on('unhandledRejection', (reason, p) => {
 process.on('uncaughtException', (err) => {
     console.log('error', 'Unhandled Exception: ' + JSON.stringify(err));
 
-    bitmex.finish(() => {
+    bitmex.mgrTrade.finish(() => {
         process.exit();
     });
 });
