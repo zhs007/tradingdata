743bd607fa2266f1f9dfdec817bb2589429ab7a3
zhs007 <sssxueren@gmail.com>
2019-01-23 15:39:03 +0800

    upd

--- startdocker_bitmex.sh
+++ startdocker_bitmex.sh
@@ -3,6 +3,7 @@ mkdir ./output/bitmex
 docker stop tradingdata_bitmex
 docker rm tradingdata_bitmex
 docker run -d \
+  --restart=always \
   --name tradingdata_bitmex \
   -v $PWD/output:/home/tradingdata/output \
   tradingdata node ./bin/bitmex.js
